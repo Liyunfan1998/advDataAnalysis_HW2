0b3457267c87974cf03afb45c9dc49a31f700edc
Liyunfan1998 <2260115144@qq.com>
2019-04-14 13:47:33 +0800

    x

--- cc2.py
+++ cc2.py
@@ -7,7 +7,7 @@
 
 import tensorflow as tf
 import numpy as np
-# import  matplotlib as
+import  matplotlib.pyplot as plt
 # cifa = input_data.read_data_sets("../cifar_10", one_hot=True)
 data_dir = '../cifar_10/cifar-10-batches-py'
 
@@ -69,12 +69,20 @@ batch_size = 128
 XtrAll, YtrAll, XteAll, YteAll = load_CIFAR10(data_dir)
 
 
-# In[5]:
+fig = plt.figure()
+ax1 = fig.add_subplot(1,1,1)
+ax1.scatter(XtrAll,YtrAll)
 
+ax2 = fig.add_subplot(1,1,2)
+ax2.scatter(XteAll,YteAll)
 
-YtrAll.shape
+ax1.xlabel("trainX")
+ax1.ylabel("trainY")
 
+ax2.xlabel("testX")
+ax2.ylabel("testY")
 
+plt.show()
 # In[6]:
 
 
@@ -125,17 +133,23 @@ def max_pool_2x2(x):
 def add_layer(inputs, w_shape, b_shape, activation_function=None,mul_func = tf.matmul,pool=None,wl=0,stddev=5e-2,norm=False):
 #     Weights = tf.Variable(tf.random_normal([in_size, out_size]))
 #     biases = tf.Variable(tf.zeros([1, out_size]))
-    Weights = weight_variable(w_shape,stddev=5e-2,wl=wl)
-    biases = bias_variable(b_shape)
-    Wx_plus_b = mul_func(inputs, Weights)+biases
-    if activation_function is None:
-        outputs = Wx_plus_b
-    else:
-        outputs = activation_function(Wx_plus_b)
-    if norm:
-        outputs= tf.nn.lrn(outputs, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75)
-    if pool is not None:
-        outputs = pool(outputs)
+    with tf.name_scope('WB'):
+        Weights = weight_variable(w_shape,stddev=5e-2,wl=wl)
+        biases = bias_variable(b_shape)
+
+    with tf.name_scope('W_mult_X_add_B'):
+        Wx_plus_b = mul_func(inputs, Weights)+biases
+    with tf.name_scope('activate'):
+        if activation_function is None:
+            outputs = Wx_plus_b
+        else:
+            outputs = activation_function(Wx_plus_b)
+    with tf.name_scope('norm'):
+        if norm:
+            outputs= tf.nn.lrn(outputs, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75)
+    with tf.name_scope('pool'):
+        if pool is not None:
+            outputs = pool(outputs)
     print ('out:',outputs.shape)
     return outputs
 
@@ -170,67 +184,65 @@ sess.run(tf.global_variables_initializer())
 
 # In[47]:
 
-
-w_shape1=[3,3,3,64]
-b_shape1=[64]
-
-x_image = tf.reshape(x,[-1, 32,32,3])
-out1 = add_layer(x_image,w_shape=w_shape1,
-                 b_shape=b_shape1,mul_func=conv2d,
-                 activation_function=tf.nn.relu, pool=max_pool_2x2)
-norm1 = tf.nn.lrn(out1, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75)
+with tf.name_scope('conv1'):
+    w_shape1=[3,3,3,64]
+    b_shape1=[64]
+    x_image = tf.reshape(x,[-1, 32,32,3])
+    out1 = add_layer(x_image,w_shape=w_shape1,
+                     b_shape=b_shape1,mul_func=conv2d,
+                     activation_function=tf.nn.relu, pool=max_pool_2x2)
+    norm1 = tf.nn.lrn(out1, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75)
 
 
 # In[48]:
 
+with tf.name_scope('conv2'):
+    w_shape2=[3,3,64,64]
+    b_shape2=[64]
 
-w_shape2=[3,3,64,64]
-b_shape2=[64]
+    out2 = add_layer(out1,w_shape=w_shape2,
+                     b_shape=b_shape2,mul_func=conv2d,
+                     activation_function=tf.nn.relu, pool=max_pool_2x2,norm=True)
 
-out2 = add_layer(out1,w_shape=w_shape2,
-                 b_shape=b_shape2,mul_func=conv2d,
-                 activation_function=tf.nn.relu, pool=max_pool_2x2,norm=True)
-
-out2_flat=tf.reshape(out2,[batch_size,-1]) # flatten
-ofdim = out2_flat.get_shape()[1].value
+    out2_flat=tf.reshape(out2,[batch_size,-1]) # flatten
+    ofdim = out2_flat.get_shape()[1].value
 # print ofdim
 
 
 # In[58]:
 
+with tf.name_scope('conv3'):
+    w_shape3=[8*8*64,1024]
+    b_shape3=[1024]
 
-w_shape3=[4096,1024]
-b_shape3=[1024]
+    # out2_flat = tf.reshape(out2,[-1,ofdim])
+    out3 = add_layer(out2_flat,w_shape=w_shape3,
+                     b_shape=b_shape3,
+                     activation_function=tf.nn.relu,stddev=0.04, wl=0.004)
 
-# out2_flat = tf.reshape(out2,[-1,ofdim])
-out3 = add_layer(out2_flat,w_shape=w_shape3,
-                 b_shape=b_shape3,
-                 activation_function=tf.nn.relu,stddev=0.04, wl=0.004)
-
-# 随机关闭一些神经元防止过拟
-keep_prob = tf.placeholder("float")
-out3 = tf.nn.dropout(out3, keep_prob)
+    # 随机关闭一些神经元防止过拟
+    keep_prob = tf.placeholder("float")
+    out3 = tf.nn.dropout(out3, keep_prob)
 
 
 # In[59]:
 
+with tf.name_scope('conv4'):
+    w_shape4 =[1024, 512]
+    b_shape4 = [512]
 
-w_shape4 =[1024, 512]
-b_shape4 = [512]
-
-out4 = add_layer(out3,w_shape4,b_shape4,activation_function=tf.nn.relu, stddev=0.04, wl=0.004)
+    out4 = add_layer(out3,w_shape4,b_shape4,activation_function=tf.nn.relu, stddev=0.04, wl=0.004)
 
 
 # In[60]:
 
+with tf.name_scope('conv_softmax'):
+    # 从1024个神经元映射到10个神经元
+    w_shape5 =[512, 10]
+    b_shape5 = [10]
 
-# 从1024个神经元映射到10个神经元
-w_shape5 =[512, 10]
-b_shape5 = [10]
+    pred = add_layer(out4,w_shape5,b_shape5,tf.nn.softmax, stddev=1/512.0)
 
-pred = add_layer(out4,w_shape5,b_shape5,tf.nn.softmax, stddev=1/512.0)
-
-pred.shape
 # labels = [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
 # b=tf.cast(labels, tf.int64)
 # sess = tf.Session()
@@ -238,25 +250,20 @@ pred.shape
 # print(sess.run(b))
 
 
-# In[56]:
-
-
-pred.shape
-
-
 # In[67]:
 
 
 y_=tf.cast(y_,tf.int32)
-cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=out4, labels=y_)
-cross_entropy_mean = tf.reduce_mean(cross_entropy)
-tf.add_to_collection('losses', cross_entropy_mean)
-loss = tf.add_n(tf.get_collection('losses'), name='total_loss')
-tf.summary.scalar('losses',loss)
-
-train_op = tf.train.AdamOptimizer(1e-3).minimize(loss)
-Xte = tf.placeholder(tf.float32, [None, 32,32,3])
-Yte = tf.placeholder(tf.int32, [None])
+with tf.name_scope('loss'):
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=out4, labels=y_)
+    cross_entropy_mean = tf.reduce_mean(cross_entropy)
+    tf.add_to_collection('losses', cross_entropy_mean)
+    loss = tf.add_n(tf.get_collection('losses'), name='total_loss')
+    tf.summary.scalar('losses',loss)
+with tf.name_scope('train'):
+    train_op = tf.train.AdamOptimizer(1e-3).minimize(loss)
+    Xte = tf.placeholder(tf.float32, [None, 32,32,3])
+    Yte = tf.placeholder(tf.int32, [None])
 # top_k_op = tf.nn.in_top_k(Xte, Yte, 1)
 
 
@@ -266,12 +273,13 @@ Yte = tf.placeholder(tf.int32, [None])
 # 计算交叉熵损失
 # cross_entropy = -tf.reduce_sum(y_*tf.log(pred))
 # # 创建优化器
-# train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 # 计算准确率， tf.argmax函数 在 label 中找出数值最大的那个元素的下标
 # correct_prediction = tf.equal(tf.argmax(pred,1), tf.argmax(y_,1))
 # accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
-top_k_op = tf.nn.in_top_k(out4, y_, 1)
-sess.run(tf.global_variables_initializer())
+with tf.name_scope('classify'):
+    top_k_op = tf.nn.in_top_k(out4, y_, 1)
+    sess.run(tf.global_variables_initializer())
 
 
 # In[20]:
@@ -287,24 +295,22 @@ import time
 
 # sess=tf.InteractiveSession()
 # sess.run(tf.global_variables_initializer())
-
-#保存模型
-saver = tf.train.Saver()
-saveFile='../cifar10/CNN_cifar10_myown.ckpt'
-
-# 使用tensorboard ，保存至LOG文件夹
-merged = tf.summary.merge_all()
-train_writer = tf.summary.FileWriter('./LOG_myown' , sess.graph)
-import random
-test_a=list()
-epoch_num=41
-n_batch = 50000 // batch_size
-#epoch：迭代次数
+with tf.name_scope('train'):
+    #保存模型
+    saver = tf.train.Saver()
+    saveFile='../cifar10/CNN_cifar10_myown_usingZYmethod.ckpt'
+    # 使用tensorboard ，保存至LOG文件夹
+    merged = tf.summary.merge_all()
+    train_writer = tf.summary.FileWriter('./LOG_myown' , sess.graph)
+    import random
+    test_a=list()
+    epoch_num=41
+    n_batch = 50000 // batch_size
+    #epoch：迭代次数
 
 
 # In[92]:
 
-"""
 for epoch in range(epoch_num):
     for batch in range(50000//batch_size + 1):
     # for batch in range(101):
@@ -340,7 +346,7 @@ for epoch in range(epoch_num):
     for batch in range(n_batch):
         #batch_ys_onehot = np.eye(10, dtype=float)[batch_labels]
         batch_images, batch_labels = get_batch(batch_size,  XtrAll,YtrAll)
-        summary,_, cross_entropy = sess.run([merged,train_step, loss], feed_dict={x_image: batch_images, y_: batch_labels, keep_prob: 1})
+        summary,_, cross_entropy = sess.run([merged,train_op, loss], feed_dict={x_image: batch_images, y_: batch_labels, keep_prob: 1})
         if batch % 100 == 0:
             print("epoch:" ,str(epoch) ," batch:",batch,'cross_entropy',cross_entropy)
         train_writer.add_summary(summary, epoch*50000+batch)
@@ -353,6 +359,7 @@ for epoch in range(epoch_num):
 train_writer.close()
 saver.save(sess, saveFile)
 
+"""
 plt.plot(range(epoch_num),test_a)
 plt.xlabel("epoch")
 plt.ylabel("test accuracy")
